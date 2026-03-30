faf6b5a5bbc18daad9ca7a6de6b116ce47b1a9b4
ajit252001 <ajitkryadav2511@gmail.com>
2026-03-30 14:40:57 +0530

    project structure ready and venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ajit Kumar Yadav",
+    author_email="ajitkryadav2511@gmail.com",
+    packages=find_packages()
+)
